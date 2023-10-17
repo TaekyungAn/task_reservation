88ed58d592ed8bc88e5cbcc28f9c5c35f9fe6d0c
TaekyungAn <atk0426@naver.com>
2023-10-17 18:52:11 +0900

    style: 글로벌 스타일 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createGlobalStyle } from "styled-components";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
 
+const GlobalStyle = createGlobalStyle`
+  html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
+    a, dl, dt, dd, ol, ul, li, form, label, table{
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 62.5%;
+        vertical-align: baseline;
+    }
+  *{
+      box-sizing: border-box;
+  }
+  body{
+    background-color: orange;
+    margin: 0 auto;
+    font-family: 'Nanum Gothic', sans-serif;
+    /* font-family: 'Lato', sans-serif; */
+  }
+  a {
+    text-decoration:none;
+    color:inherit;
+  }
+  button{
+    cursor: pointer;
+  }
+  `;
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
