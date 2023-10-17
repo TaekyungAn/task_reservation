424565cdc742e20e54c2873daa8817df7224f4ce
TaekyungAn <atk0426@naver.com>
2023-10-17 14:58:36 +0900

    style: 페이지 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NewRes from "./pages/NewRes";
+import EditRes from "./pages/EditRes";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <AppContainer>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new_res" element={<NewRes />} />
+          <Route path="/edit_res" element={<EditRes />} />
+        </Routes>
+      </BrowserRouter>
+    </AppContainer>
+  );
 }
 
 export default App;
