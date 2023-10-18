8ae37cafc235d5a8fad0495a12af561abee5f867
TaekyungAn <atk0426@naver.com>
2023-10-18 14:30:45 +0900

    style: 헤더 프롭스네임 수정

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -4,7 +4,7 @@ import CloseBtn from "../CommonUI/CloseBtn";
 
 const HeaderContainer = styled.div`
   width: 100%;
-  height: 8rem;
+  height: 8.6rem;
 
   position: relative;
   display: flex;
@@ -16,7 +16,6 @@ const HeaderContainer = styled.div`
 
   span {
     font-size: 2.4rem;
-    font-weight: 600;
     color: ${(props) => props.theme.gray.font_color};
   }
 `;
@@ -29,10 +28,14 @@ const LeftBtn = styled.div`
 
   left: 2rem;
   height: 100%;
-  background-color: aliceblue;
 `;
 const RightBtn = styled.div`
   position: absolute;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
   right: 2rem;
 `;
 
@@ -41,11 +44,11 @@ function Header() {
     <HeaderContainer>
       <LeftBtn>
         <Button
-          width={"18rem"}
-          height={"65%"}
-          fontsize={"1.6rem"}
-          bgcolor={"white"}
-          textcolor={"orange"}
+          $width={"18rem"}
+          $height={"5rem"}
+          $fontSize={"1.6rem"}
+          $bgcolor={"white"}
+          $textcolor={"orange"}
         >
           {/* 아이콘 경로 설정 안됨!!! */}
           {/* <img src="../../assets/icons/add.svg" alt="추가 아이콘" /> */}
@@ -59,8 +62,8 @@ function Header() {
             <g id=" Round / Content / add">
               <path
                 id="&#240;&#159;&#148;&#185;Icon Color"
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M18 13H13V18C13 18.55 12.55 19 12 19C11.45 19 11 18.55 11 18V13H6C5.45 13 5 12.55 5 12C5 11.45 5.45 11 6 11H11V6C11 5.45 11.45 5 12 5C12.55 5 13 5.45 13 6V11H18C18.55 11 19 11.45 19 12C19 12.55 18.55 13 18 13Z"
                 fill="#EC551F"
               />
