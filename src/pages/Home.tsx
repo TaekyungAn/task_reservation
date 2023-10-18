23a41471726e2b5ca6deefffc0e834ae6b5ae705
TaekyungAn <atk0426@naver.com>
2023-10-18 14:31:41 +0900

    style: 카드 컴포넌트 시험

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,38 @@
+import styled from "styled-components";
+import Header from "../components/Home/Header";
+import Card from "../components/Home/Card";
+
+const HomeContainer = styled.header`
+  background-color: ${(props) => props.theme.gray.home_background};
+  width: 100%;
+  height: 100%;
+  min-height: 30rem;
+  border-radius: 1.1rem;
+  overflow: scroll;
+`;
+
+const Cards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-auto-rows: minmax(20rem, auto);
+  gap: 1.8rem;
+
+  padding: 1.8rem;
+`;
+
 function Home() {
-  return <div className="Home">Home</div>;
+  return (
+    <HomeContainer>
+      <Header />
+      <Cards>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Cards>
+    </HomeContainer>
+  );
 }
 
 export default Home;
