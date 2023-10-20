887131b15c7c155a0ad4c95be70375788f023450
TaekyungAn <atk0426@naver.com>
2023-10-20 19:02:34 +0900

    feat: 전달받은 데이터 페인팅

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,23 @@
 import * as S from "./styles/Home.styled";
 import { HCard, HHeader } from "../components/Home";
 import { useLocation } from "react-router-dom";
-import { IDummyList } from "../data/datalist";
+import { IDummyList, dummyList } from "../data/datalist";
+import { useContext } from "react";
+import { ResStateContext } from "../reducer";
 
 function Home() {
-  const location = useLocation();
-  const resInfo = { ...location.state };
-  console.log(resInfo);
-
-  // console.log("home", resInfo);
-  // useEffect(() => {
-  //   setData([resInfo, ...data]);
-  // }, []);
+  // const location = useLocation();
+  // const resInfo = { ...location.state };
+  // console.log(resInfo);
+  const resList = useContext(ResStateContext);
+  console.log(resList);
 
   return (
     <S.HomeContainer>
       <HHeader />
       <S.Cards>
-        {resInfo.length > 0
-          ? resInfo.map((item: IDummyList) => {
+        {resList.length > 0
+          ? resList.map((item: any) => {
               return <HCard key={item.id} {...item} />;
             })
           : "새로운 예약이 없습니다"}
