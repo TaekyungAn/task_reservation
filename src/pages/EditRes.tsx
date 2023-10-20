0b13de60ae52c07c722e6fe9809a9f4a07a0793d
TaekyungAn <atk0426@naver.com>
2023-10-20 19:04:15 +0900

    style: Edit컴포넌트 작성

--- src/pages/EditRes.tsx
+++ src/pages/EditRes.tsx
@@ -1,5 +1,174 @@
+import * as S from "./styles/EditRes.styled";
+import { useContext, useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { IDummyList, dummyList } from "../data/datalist";
+import { RHeader } from "../components/Reservation";
+import { CButton, CIcon } from "../components/CommonUI";
+import { ResDispatchContext } from "../reducer";
+
 function EditRes() {
-  return <div className="EditRes">EditRes</div>;
+  const { onCreate } = useContext(ResDispatchContext);
+
+  const navigate = useNavigate();
+  const [state, setState] = useState({
+    name: "",
+    phone: "",
+    reserved_date: 0,
+    reserved_time: 0,
+    created_date: new Date().toLocaleDateString(),
+    guests: 0,
+    table: 0,
+    note: "",
+  });
+
+  const [guestNum, setGeustNum] = useState(0);
+  const nameInput = useRef<HTMLInputElement>(null);
+  const phoneInput = useRef<HTMLInputElement>(null);
+
+  // 게스트 숫자 업데이트
+  useEffect(() => {
+    setState({
+      ...state,
+      guests: guestNum,
+    });
+  }, [guestNum]);
+
+  // 게스트 숫자 감소 버튼 함수
+  const handleDecrease = (e: React.MouseEvent) => {
+    e.preventDefault();
+    if (guestNum > 0) setGeustNum((prev) => prev - 1);
+  };
+
+  // 입력창 (이름, 번호, 메모) 변화 내용 저장
+  const handleChangeState = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+      | React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const dataId = useRef(0);
+  const [data, setData] = useState<IDummyList[]>([...dummyList]);
+
+  // 폼 제출
+  const handleSubmit = (e: React.MouseEvent) => {
+    e.preventDefault();
+    if (state.name.length < 1) {
+      nameInput.current?.focus();
+      return;
+    }
+    if (state.phone.length < 10) {
+      phoneInput.current?.focus();
+      return;
+    }
+    if (state.guests === 0) {
+      alert("손님은 최소 1명 이상 입력해주세요");
+      return;
+    }
+    if (window.confirm("예약을 완료하시겠습니까?")) {
+      onCreate(
+        state.created_date,
+        state.name,
+        state.phone,
+        state.guests,
+        state.note,
+        state.table,
+        state.reserved_date,
+        state.reserved_time
+      );
+      navigate("/");
+    }
+  };
+
+  return (
+    <S.EditResContainer>
+      <RHeader />
+      <S.Information>
+        <S.PersonalInfo>
+          <input
+            placeholder="Name"
+            name="name"
+            value={state.name}
+            onChange={handleChangeState}
+            ref={nameInput}
+          />
+          <input
+            placeholder="Phone"
+            name="phone"
+            value={state.phone}
+            onChange={handleChangeState}
+            ref={phoneInput}
+          />
+          <CButton $width={"25rem"} $bgcolor={"white"} $textcolor={"black"}>
+            <CIcon $name={"event_available"} />
+            Select Date
+          </CButton>
+        </S.PersonalInfo>
+        <S.PartyInfo>
+          <S.GuestesNumber>
+            <span>Guests</span>
+            <CButton
+              $width={"5rem"}
+              $bgcolor={"white"}
+              $textcolor={"black"}
+              onClick={handleDecrease}
+            >
+              <CIcon $name={"math-minus"} />
+            </CButton>
+            <span className="num">{guestNum}</span>
+            <CButton
+              $width={"5rem"}
+              $bgcolor={"white"}
+              $textcolor={"black"}
+              onClick={(e) => {
+                e.preventDefault();
+                setGeustNum((prev) => prev + 1);
+              }}
+            >
+              <CIcon $name={"math-plus"} />
+            </CButton>
+          </S.GuestesNumber>
+          <S.TableInfo>
+            <select
+              name="table"
+              value={state.table}
+              onChange={handleChangeState}
+            >
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => {
+                return (
+                  <option value={item} key={item}>
+                    table {item}
+                  </option>
+                );
+              })}
+            </select>
+          </S.TableInfo>
+        </S.PartyInfo>
+        <S.AdttionalInfo>
+          <textarea
+            placeholder="memo"
+            value={state.note}
+            name="note"
+            onChange={handleChangeState}
+          />
+        </S.AdttionalInfo>
+        <CButton
+          $height={"7rem"}
+          $width={"100%"}
+          $bgcolor={"orange"}
+          $textcolor={"white"}
+          onClick={handleSubmit}
+        >
+          Save
+        </CButton>
+      </S.Information>
+    </S.EditResContainer>
+  );
 }
 
 export default EditRes;
---
+++ src/pages/styles/EditRes.styled.ts
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const EditResContainer = styled.header`
+  background-color: ${(props) => props.theme.gray.home_background};
+  width: 100%;
+  height: 100%;
+  min-height: 30rem;
+  border-radius: 1.1rem;
+  overflow: scroll;
+  font-size: 1.6rem;
+`;
+export const Information = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 3rem;
+
+  & > div {
+    margin-bottom: 3rem;
+  }
+`;
+export const PersonalInfo = styled.div`
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+  justify-content: space-between;
+  > input {
+    height: 6rem;
+    flex-grow: 1;
+    border-radius: 1rem;
+    border: 1px gray solid;
+  }
+`;
+export const DateInfo = styled.div``;
+export const PartyInfo = styled.div`
+  display: flex;
+`;
+export const GuestesNumber = styled.div`
+  display: flex;
+  & > .num {
+    font-size: 3.2rem;
+  }
+`;
+export const TableInfo = styled.div``;
+export const AdttionalInfo = styled.div`
+  & textarea {
+    height: 18rem;
+  }
+`;
